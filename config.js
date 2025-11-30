51e6f9f3e671f66a1294a635c37784866f94b653
MRezor <mrezorfr@gmail.com>
2025-11-30 08:51:19 +0100

    Add config.js to version control

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     bot: {
-        token: 'YOUR TOKEN',
+        token: '',
         activity: 'by MRezor ❤️',
         name: 'by MRezor ❤️',
         logo: 'https://images-ext-1.discordapp.net/external/7CTpaPKCXMBYnDPh2pu9sic-1568RjXelLnUPCEIw-g/https/cdn.discordapp.com/avatars/843796161407746089/45d9acd8917467a6f2de52205655908b.webp',
